571c7fea4becfa4a1082d4021baa3fd919842157
juan <130606010+juancastrop@users.noreply.github.com>
2025-09-16 00:10:09 +0000

    creacion y aplicacion de  variables y ramdoms

--- src/app.js
+++ src/app.js
@@ -5,7 +5,19 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
+window.onload = function () {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let who = ['The dog', 'My grandma', 'The mailman', 'My bird'];
+  let action = ['ate', 'peed', 'crushed', 'broke'];
+  let what = ['my homework', 'my phone', 'the car'];
+  let when = ['before the class', 'when I was sleeping', 'while I was exercising', 'during my lunch', 'while I was praying'];
+  let aleatorioWho = Math.floor(Math.random() * who.length)
+  let aleatorioAction = Math.floor(Math.random() * action.length)
+  let aleatorioWhat = Math.floor(Math.random() * what.length)
+  let aleatorioWhen = Math.floor(Math.random() * when.length)
+  let excusa = who[aleatorioWho] + " " + action[aleatorioAction] + " " + what[aleatorioWhat] + " " + when[aleatorioWhen]
+  let element = document.querySelector("#excuse")
+  element.innerText = excusa
+
+  console.log("Hello boys from the console!");
 };
